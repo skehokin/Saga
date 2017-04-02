c8c86d56efe13f75a7b914a551cb2a19d9b8ce79
Siobhan Hokin <skehokin@gmail.com>
2017-04-02 18:56:00 +1000

    add styles to form

--- blogMain.py
+++ blogMain.py
@@ -7,8 +7,9 @@ from google.appengine.ext import db
 template_dir = os.path.join(os.path.dirname(__file__),'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
 
-
+class BlogEntries
 class Handler(webapp2.RequestHandler):
+    
     def write(self,*a,**kw):
         self.response.out.write(*a,**kw)
         
@@ -26,6 +27,9 @@ class MainPage(Handler):
 class NewPost(Handler):
     def get(self):
         self.render("newpage.html")
+        subject=self.request.get('subject')
+        content=self.request.get('content')
+        
 
 
 app=webapp2.WSGIApplication([('/', MainPage),('/newpost',NewPost)],debug=True)
